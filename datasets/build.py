47c2dccf1e945f964a1dd9c25df957074f89f24e
Duc <ductrong040704@gmail.com>
2026-03-10 13:53:29 +0700

    u load dataset

--- datasets/build.py
+++ datasets/build.py
@@ -1,20 +1,28 @@
 import logging
+import random
+
+import numpy as np
 import torch
 import torchvision.transforms as T
 from torch.utils.data import DataLoader
+
 from datasets.sampler import RandomIdentitySampler
 from datasets.sampler_ddp import RandomIdentitySampler_DDP
-import random
 from utils.comm import get_world_size
-import numpy as np
-from .bases import ImageDataset, TextDataset, ImageTextDataset
 
+from .bases import ImageDataset, TextDataset, ImageTextDataset
 from .cuhkpedes import CUHKPEDES
 from .icfgpedes import ICFGPEDES
 from .rstpreid import RSTPReid
 from .market1501 import Market1501
 
-__factory = {'CUHK-PEDES': CUHKPEDES, 'ICFG-PEDES': ICFGPEDES, 'RSTPReid': RSTPReid, 'Market1501': Market1501}
+
+__factory = {
+    'CUHK-PEDES': CUHKPEDES,
+    'ICFG-PEDES': ICFGPEDES,
+    'RSTPReid': RSTPReid,
+    'Market1501': Market1501
+}
 
 
 def build_transforms(img_size=(384, 128), aug=False, is_train=True,
@@ -32,7 +40,6 @@ def build_transforms(img_size=(384, 128), aug=False, is_train=True,
         ])
         return transform
 
-    # transform for training
     if aug:
         transform = T.Compose([
             T.Resize((height, width)),
@@ -53,10 +60,8 @@ def build_transforms(img_size=(384, 128), aug=False, is_train=True,
     return transform
 
 
-
 def collate(batch):
     keys = set([key for b in batch for key in b.keys()])
-    # turn list of dicts data structure to dict of lists data structure
     dict_batch = {k: [dic[k] if k in dic else None for dic in batch] for k in keys}
 
     batch_tensor_dict = {}
@@ -64,11 +69,47 @@ def collate(batch):
         if isinstance(v[0], (int, np.int64)):
             batch_tensor_dict.update({k: torch.tensor(v)})
         elif torch.is_tensor(v[0]):
-             batch_tensor_dict.update({k: torch.stack(v)})
+            batch_tensor_dict.update({k: torch.stack(v)})
         else:
             raise TypeError(f"Unexpect data type: {type(v[0])} in a batch.")
 
     return batch_tensor_dict
+
+
+def _build_refer_loader(dataset, args, num_workers):
+    random.seed(42)
+
+    all_text = []
+    all_id = []
+
+    # dataset.train is expected as list of tuples: (pid, image_id, img_path, caption)
+    for pid, _, _, text in dataset.train:
+        all_id.append(pid)
+        all_text.append(text)
+
+    if len(all_text) == 0:
+        raise RuntimeError("No training captions found to build refer_txt_loader.")
+
+    sample_size = max(1, int(len(all_text) * 0.2))
+    sample_size = min(sample_size, len(all_text))
+    random_indices = random.sample(range(len(all_text)), sample_size)
+
+    sampled_ids = [all_id[i] for i in random_indices]
+    sampled_texts = [all_text[i] for i in random_indices]
+
+    refer = TextDataset(
+        sampled_ids,
+        sampled_texts,
+        text_length=args.text_length
+    )
+
+    refer_txt_loader = DataLoader(
+        refer,
+        batch_size=args.test_batch_size,
+        shuffle=False,
+        num_workers=num_workers
+    )
+    return refer_txt_loader
 
 
 def build_dataloader(args, tranforms=None):
@@ -77,116 +118,142 @@ def build_dataloader(args, tranforms=None):
     num_workers = args.num_workers
     dataset = __factory[args.dataset_name](root=args.root_dir)
     num_classes = len(dataset.train_id_container)
-    random.seed(42)
-    ds1 = dataset.train
-    all_text = []
-    all_id = []
-    for a_, b_, c_, text in ds1:
-        all_id.append(a_)
-        all_text.append(text)
-    sample_size = int(len(all_text) * 0.2)
-    random_indices = random.sample(range(len(all_text)), sample_size)
 
-    sampled_ids = [all_id[i] for i in random_indices]
-    sampled_texts = [all_text[i] for i in random_indices]
-    refer = TextDataset(sampled_ids,
-                        sampled_texts,
-                        text_length=args.text_length)
+    refer_txt_loader = _build_refer_loader(dataset, args, num_workers)
 
-    refer_txt_loader = DataLoader(refer,
-                                  batch_size=args.test_batch_size,
-                                  shuffle=False,
-                                  num_workers=num_workers)
-    
     if args.training:
-        train_transforms = build_transforms(img_size=args.img_size,
-                                            aug=args.img_aug,
-                                            is_train=True,
-                                            flip_prob=args.flip_prob,
-                                            pad_size=args.pad_size,
-                                            re_prob=args.re_prob)
-        val_transforms = build_transforms(img_size=args.img_size,
-                                          is_train=False)
+        train_transforms = build_transforms(
+            img_size=args.img_size,
+            aug=args.img_aug,
+            is_train=True,
+            flip_prob=args.flip_prob,
+            pad_size=args.pad_size,
+            re_prob=args.re_prob
+        )
+        val_transforms = build_transforms(
+            img_size=args.img_size,
+            is_train=False
+        )
 
-
-        train_set = ImageTextDataset(dataset.train,args,
-                            train_transforms,
-                        text_length=args.text_length)
+        train_set = ImageTextDataset(
+            dataset.train,
+            args,
+            train_transforms,
+            text_length=args.text_length
+        )
 
         if args.sampler == 'identity':
             if args.distributed:
                 logger.info('using ddp random identity sampler')
                 logger.info('DISTRIBUTED TRAIN START')
-                mini_batch_size = args.batch_size // get_world_size()
-                # TODO wait to fix bugs
-                data_sampler = RandomIdentitySampler_DDP(
-                    dataset.train, args.batch_size, args.num_instance)
-                batch_sampler = torch.utils.data.sampler.BatchSampler(
-                    data_sampler, mini_batch_size, True)
 
+                mini_batch_size = args.batch_size // get_world_size()
+                data_sampler = RandomIdentitySampler_DDP(
+                    dataset.train, args.batch_size, args.num_instance
+                )
+                batch_sampler = torch.utils.data.sampler.BatchSampler(
+                    data_sampler, mini_batch_size, True
+                )
+                train_loader = DataLoader(
+                    train_set,
+                    batch_sampler=batch_sampler,
+                    num_workers=num_workers,
+                    collate_fn=collate
+                )
             else:
                 logger.info(
-                    f'using random identity sampler: batch_size: {args.batch_size}, id: {args.batch_size // args.num_instance}, instance: {args.num_instance}'
+                    f'using random identity sampler: batch_size: {args.batch_size}, '
+                    f'id: {args.batch_size // args.num_instance}, '
+                    f'instance: {args.num_instance}'
                 )
-                train_loader = DataLoader(train_set,
-                                          batch_size=args.batch_size,
-                                          sampler=RandomIdentitySampler(
-                                              dataset.train, args.batch_size,
-                                              args.num_instance),
-                                          num_workers=num_workers,
-                                          collate_fn=collate)
+                train_loader = DataLoader(
+                    train_set,
+                    batch_size=args.batch_size,
+                    sampler=RandomIdentitySampler(
+                        dataset.train, args.batch_size, args.num_instance
+                    ),
+                    num_workers=num_workers,
+                    collate_fn=collate
+                )
+
         elif args.sampler == 'random':
-            # TODO add distributed condition
             logger.info('using random sampler')
-            train_loader = DataLoader(train_set,
-                                      batch_size=args.batch_size,
-                                      shuffle=True,
-                                      num_workers=num_workers,
-                                      collate_fn=collate)
+            train_loader = DataLoader(
+                train_set,
+                batch_size=args.batch_size,
+                shuffle=True,
+                num_workers=num_workers,
+                collate_fn=collate
+            )
         else:
-            logger.error('unsupported sampler! expected softmax or triplet but got {}'.format(args.sampler))
+            raise ValueError(
+                f"unsupported sampler! expected identity or random but got {args.sampler}"
+            )
 
-        # use test set as validate set
+        # For Market1501:
+        #   dataset.val  -> query captions/images
+        #   dataset.test -> gallery captions/images
         ds = dataset.val if args.val_dataset == 'val' else dataset.test
-        val_img_set = ImageDataset(ds['image_pids'], ds['img_paths'],
-                                   val_transforms)
-        val_txt_set = TextDataset(ds['caption_pids'],
-                                  ds['captions'],
-                                  text_length=args.text_length)
 
-        val_img_loader = DataLoader(val_img_set,
-                                    batch_size=args.batch_size,
-                                    shuffle=False,
-                                    num_workers=num_workers)
-        val_txt_loader = DataLoader(val_txt_set,
-                                    batch_size=args.batch_size,
-                                    shuffle=False,
-                                    num_workers=num_workers)
+        val_img_set = ImageDataset(
+            ds['image_pids'],
+            ds['img_paths'],
+            val_transforms
+        )
+        val_txt_set = TextDataset(
+            ds['caption_pids'],
+            ds['captions'],
+            text_length=args.text_length
+        )
+
+        val_img_loader = DataLoader(
+            val_img_set,
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=num_workers
+        )
+        val_txt_loader = DataLoader(
+            val_txt_set,
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=num_workers
+        )
 
         return train_loader, val_img_loader, val_txt_loader, refer_txt_loader, num_classes
 
     else:
-        # build dataloader for testing
         if tranforms:
             test_transforms = tranforms
         else:
-            test_transforms = build_transforms(img_size=args.img_size,
-                                               is_train=False)
+            test_transforms = build_transforms(
+                img_size=args.img_size,
+                is_train=False
+            )
 
         ds = dataset.test
-        test_img_set = ImageDataset(ds['image_pids'], ds['img_paths'],
-                                    test_transforms)
-        test_txt_set = TextDataset(ds['caption_pids'],
-                                   ds['captions'],
-                                   text_length=args.text_length)
 
-        test_img_loader = DataLoader(test_img_set,
-                                     batch_size=args.test_batch_size,
-                                     shuffle=False,
-                                     num_workers=num_workers)
-        test_txt_loader = DataLoader(test_txt_set,
-                                     batch_size=args.test_batch_size,
-                                     shuffle=False,
-                                     num_workers=num_workers)
+        test_img_set = ImageDataset(
+            ds['image_pids'],
+            ds['img_paths'],
+            test_transforms
+        )
+        test_txt_set = TextDataset(
+            ds['caption_pids'],
+            ds['captions'],
+            text_length=args.text_length
+        )
+
+        test_img_loader = DataLoader(
+            test_img_set,
+            batch_size=args.test_batch_size,
+            shuffle=False,
+            num_workers=num_workers
+        )
+        test_txt_loader = DataLoader(
+            test_txt_set,
+            batch_size=args.test_batch_size,
+            shuffle=False,
+            num_workers=num_workers
+        )
 
         return test_img_loader, test_txt_loader, refer_txt_loader, num_classes
--- datasets/market1501.py
+++ datasets/market1501.py
@@ -1,146 +1,310 @@
+# encoding: utf-8
+import glob
+import json
 import os.path as op
 import re
-import json
+from typing import Dict, List, Any
 
 from .bases import BaseDataset
 
 
 class Market1501(BaseDataset):
     """
-    Market1501 with text captions for text-to-image person re-identification.
+    Market1501 with captions for text-image retrieval style loading.
 
-    Reference:
-    Zheng et al. Scalable Person Re-identification: A Benchmark. ICCV 2015.
+    Expected image folders:
+        {root}/Market-1501-v15.09.15/bounding_box_train
+        {root}/Market-1501-v15.09.15/query
+        {root}/Market-1501-v15.09.15/bounding_box_test
 
-    Dataset statistics:
-    # identities: 1501 (+1 for background)
-    # images: 12936 (train) + 3368 (query) + 15913 (gallery)
+    Expected caption files (placed directly under {root}):
+        market1501_bounding_box_train[.jsonl|.json]
+        market1501_query[.jsonl|.json]
+        market1501_bounding_box_test[.jsonl|.json]
 
-    Caption annotations are read from JSONL files located alongside this module.
-    Each line: {"image_path": "XXXX_cXsX_XXXXXX_XX.jpg", "caption": "..."}
+    Supported caption-file styles:
+    1) JSONL: one record per line
+    2) JSON: list[dict]
+    3) JSON: dict mapping filename -> caption(s)
+
+    Each record can use common keys like:
+        image key: img_path / image_path / file_path / filename / file_name / image / img / path / name
+        caption key: captions / caption / texts / text / description / descriptions
     """
     dataset_dir = 'Market-1501-v15.09.15'
 
-    def __init__(self, root='', verbose=True):
+    def __init__(self, root='', verbose=True, **kwargs):
         super(Market1501, self).__init__()
+
+        self.root = root
         self.dataset_dir = op.join(root, self.dataset_dir)
         self.train_dir = op.join(self.dataset_dir, 'bounding_box_train')
         self.query_dir = op.join(self.dataset_dir, 'query')
         self.gallery_dir = op.join(self.dataset_dir, 'bounding_box_test')
 
-        # JSONL caption files located alongside this Python file
-        jsonl_dir = op.dirname(op.abspath(__file__))
-        self.train_jsonl = op.join(jsonl_dir, 'market1501_bounding_box_train.jsonl')
-        self.test_jsonl = op.join(jsonl_dir, 'market1501_bounding_box_test.jsonl')
-        self.query_jsonl = op.join(jsonl_dir, 'market1501_query.jsonl')
+        self.train_anno_path = self._resolve_caption_file('market1501_bounding_box_train')
+        self.query_anno_path = self._resolve_caption_file('market1501_query')
+        self.gallery_anno_path = self._resolve_caption_file('market1501_bounding_box_test')
 
         self._check_before_run()
 
-        # Read annotations from JSONL
-        train_annos = self._read_jsonl(self.train_jsonl, self.train_dir)
-        gallery_annos = self._read_jsonl(self.test_jsonl, self.gallery_dir)
-        query_annos = self._read_jsonl(self.query_jsonl, self.query_dir)
+        self.train, self.train_id_container = self._process_split(
+            img_dir=self.train_dir,
+            anno_path=self.train_anno_path,
+            training=True,
+            relabel=True
+        )
+        self.val, self.val_id_container = self._process_split(
+            img_dir=self.query_dir,
+            anno_path=self.query_anno_path,
+            training=False,
+            relabel=False
+        )
+        self.test, self.test_id_container = self._process_split(
+            img_dir=self.gallery_dir,
+            anno_path=self.gallery_anno_path,
+            training=False,
+            relabel=False
+        )
 
-        self.train_annos = train_annos
-        self.test_annos = gallery_annos
-        self.val_annos = self.test_annos
-
-        # Process splits
-        self.train, self.train_id_container = self._process_train(train_annos)
-        self.test, self.test_id_container = self._process_test(query_annos, gallery_annos)
-        self.val = self.test
-        self.val_id_container = self.test_id_container
+        # Optional aliases if some other code wants query/gallery names
+        self.query = self.val
+        self.gallery = self.test
 
         if verbose:
             self.logger.info("=> Market1501 Images and Captions are loaded")
             self.show_dataset_info()
 
-    def _read_jsonl(self, jsonl_path, img_dir):
-        """Read JSONL caption file and return list of annotation dicts.
-        Skips junk images (pid == -1).
-        """
-        annos = []
+    def _resolve_caption_file(self, stem: str) -> str:
+        candidates = [
+            op.join(self.root, stem),
+            op.join(self.root, stem + '.jsonl'),
+            op.join(self.root, stem + '.json'),
+        ]
+        for p in candidates:
+            if op.exists(p):
+                return p
+        raise RuntimeError(
+            f"Caption file for '{stem}' not found. Tried: {candidates}"
+        )
+
+    def _check_before_run(self):
+        if not op.exists(self.dataset_dir):
+            raise RuntimeError(f"'{self.dataset_dir}' is not available")
+        if not op.exists(self.train_dir):
+            raise RuntimeError(f"'{self.train_dir}' is not available")
+        if not op.exists(self.query_dir):
+            raise RuntimeError(f"'{self.query_dir}' is not available")
+        if not op.exists(self.gallery_dir):
+            raise RuntimeError(f"'{self.gallery_dir}' is not available")
+        if not op.exists(self.train_anno_path):
+            raise RuntimeError(f"'{self.train_anno_path}' is not available")
+        if not op.exists(self.query_anno_path):
+            raise RuntimeError(f"'{self.query_anno_path}' is not available")
+        if not op.exists(self.gallery_anno_path):
+            raise RuntimeError(f"'{self.gallery_anno_path}' is not available")
+
+    def _extract_pid_camid(self, img_path: str):
         pattern = re.compile(r'([-\d]+)_c(\d)')
-        with open(jsonl_path, 'r', encoding='utf-8') as f:
+        m = pattern.search(op.basename(img_path))
+        if m is None:
+            raise RuntimeError(f"Cannot parse pid/camid from: {img_path}")
+        pid, camid = map(int, m.groups())
+        return pid, camid - 1  # camid to 0-based
+
+    def _load_annotation_records(self, anno_path: str) -> List[Any]:
+        with open(anno_path, 'r', encoding='utf-8') as f:
+            raw = f.read().strip()
+
+        if not raw:
+            return []
+
+        # Try normal JSON first
+        try:
+            obj = json.loads(raw)
+            if isinstance(obj, list):
+                return obj
+            if isinstance(obj, dict):
+                if 'annotations' in obj and isinstance(obj['annotations'], list):
+                    return obj['annotations']
+                if 'data' in obj and isinstance(obj['data'], list):
+                    return obj['data']
+                # dict mapping filename -> caption(s)
+                records = []
+                for k, v in obj.items():
+                    records.append({
+                        'img_path': k,
+                        'captions': v
+                    })
+                return records
+        except json.JSONDecodeError:
+            pass
+
+        # Fallback: JSONL
+        records = []
+        with open(anno_path, 'r', encoding='utf-8') as f:
             for line in f:
                 line = line.strip()
                 if not line:
                     continue
-                entry = json.loads(line)
-                image_name = entry['image_path']
-                caption = entry['caption']
+                records.append(json.loads(line))
+        return records
 
-                match = pattern.search(image_name)
-                pid = int(match.group(1))
-                camid = int(match.group(2))
+    def _extract_image_name(self, rec: Any) -> str:
+        if isinstance(rec, str):
+            return op.basename(rec)
 
-                if pid == -1:
-                    continue  # skip junk images
+        if not isinstance(rec, dict):
+            return None
 
-                img_path = op.join(img_dir, image_name)
-                annos.append({
-                    'pid': pid,
-                    'camid': camid,
-                    'img_path': img_path,
-                    'caption': caption,
-                })
-        return annos
+        image_keys = [
+            'img_path', 'image_path', 'file_path', 'filename',
+            'file_name', 'image', 'img', 'path', 'name'
+        ]
+        for k in image_keys:
+            if k in rec and rec[k]:
+                return op.basename(str(rec[k]))
 
-    def _process_train(self, annos):
-        """Process training annotations into (pid, image_id, img_path, caption) tuples.
-        PIDs are relabeled to be contiguous starting from 0.
-        """
-        pid_container = set()
-        for anno in annos:
-            pid_container.add(anno['pid'])
+        return None
 
-        pid2label = {pid: label for label, pid in enumerate(sorted(pid_container))}
+    def _extract_captions(self, rec: Any) -> List[str]:
+        if isinstance(rec, str):
+            return [rec.strip()] if rec.strip() else []
 
-        dataset = []
-        image_id = 0
-        for anno in annos:
-            pid = pid2label[anno['pid']]
-            dataset.append((pid, image_id, anno['img_path'], anno['caption']))
-            image_id += 1
+        if not isinstance(rec, dict):
+            return []
 
-        relabeled_pids = set(pid2label.values())
-        return dataset, relabeled_pids
+        caption_keys = [
+            'captions', 'caption', 'texts', 'text',
+            'description', 'descriptions'
+        ]
 
-    def _process_test(self, query_annos, gallery_annos):
-        """Process test data: gallery images as image set, query captions as text set."""
-        pid_container = set()
+        value = None
+        for k in caption_keys:
+            if k in rec:
+                value = rec[k]
+                break
 
-        img_paths = []
-        image_pids = []
+        if value is None:
+            return []
+
+        if isinstance(value, str):
+            value = [value]
+        elif not isinstance(value, list):
+            value = [value]
+
         captions = []
-        caption_pids = []
+        for x in value:
+            if isinstance(x, str):
+                x = x.strip()
+                if x:
+                    captions.append(x)
+            elif x is not None:
+                x = str(x).strip()
+                if x:
+                    captions.append(x)
 
-        # Gallery images
-        for anno in gallery_annos:
-            pid = anno['pid']
-            pid_container.add(pid)
-            img_paths.append(anno['img_path'])
-            image_pids.append(pid)
+        return captions
 
-        # Query captions
-        for anno in query_annos:
-            pid = anno['pid']
-            pid_container.add(pid)
-            captions.append(anno['caption'])
-            caption_pids.append(pid)
+    def _load_caption_map(self, anno_path: str) -> Dict[str, List[str]]:
+        records = self._load_annotation_records(anno_path)
+        caption_map = {}
 
-        dataset = {
-            "image_pids": image_pids,
-            "img_paths": img_paths,
-            "caption_pids": caption_pids,
-            "captions": captions,
-        }
+        for rec in records:
+            img_name = self._extract_image_name(rec)
+            captions = self._extract_captions(rec)
+
+            if img_name is None:
+                continue
+            if len(captions) == 0:
+                continue
+
+            if img_name not in caption_map:
+                caption_map[img_name] = []
+            caption_map[img_name].extend(captions)
+
+        return caption_map
+
+    def _process_split(self, img_dir: str, anno_path: str, training=False, relabel=False):
+        img_paths = sorted(glob.glob(op.join(img_dir, '*.jpg')))
+        caption_map = self._load_caption_map(anno_path)
+
+        # collect valid image paths first
+        valid_img_paths = []
+        original_pid_container = set()
+
+        for img_path in img_paths:
+            pid, _ = self._extract_pid_camid(img_path)
+            if pid == -1:
+                continue
+            original_pid_container.add(pid)
+            valid_img_paths.append(img_path)
+
+        pid2label = None
+        if relabel:
+            pid2label = {pid: label for label, pid in enumerate(sorted(original_pid_container))}
+
+        missing_caption_images = []
+
+        if training:
+            dataset = []
+            image_id = 0
+
+            for img_path in valid_img_paths:
+                img_name = op.basename(img_path)
+                pid, _ = self._extract_pid_camid(img_path)
+                captions = caption_map.get(img_name, [])
+
+                if len(captions) == 0:
+                    missing_caption_images.append(img_name)
+                    image_id += 1
+                    continue
+
+                pid_out = pid2label[pid] if relabel else pid
+
+                for caption in captions:
+                    dataset.append((pid_out, image_id, img_path, caption))
+
+                image_id += 1
+
+            pid_container = set(pid2label.values()) if relabel else set(original_pid_container)
+
+            if relabel:
+                expected = set(range(len(pid_container)))
+                assert pid_container == expected, \
+                    f"Train relabel is not contiguous: got {sorted(pid_container)[:10]} ..."
+
+        else:
+            dataset = {
+                "image_pids": [],
+                "img_paths": [],
+                "caption_pids": [],
+                "captions": []
+            }
+
+            for img_path in valid_img_paths:
+                img_name = op.basename(img_path)
+                pid, _ = self._extract_pid_camid(img_path)
+                captions = caption_map.get(img_name, [])
+
+                dataset["image_pids"].append(pid)
+                dataset["img_paths"].append(img_path)
+
+                if len(captions) == 0:
+                    missing_caption_images.append(img_name)
+                    continue
+
+                for caption in captions:
+                    dataset["captions"].append(caption)
+                    dataset["caption_pids"].append(pid)
+
+            pid_container = set(original_pid_container)
+
+        if len(missing_caption_images) > 0:
+            self.logger.warning(
+                f"[Market1501] {len(missing_caption_images)} images in '{op.basename(img_dir)}' "
+                f"have no matched captions from '{op.basename(anno_path)}'. "
+                f"First 10: {missing_caption_images[:10]}"
+            )
+
         return dataset, pid_container
-
-    def _check_before_run(self):
-        """Check if all files are available before going deeper."""
-        for path in [self.dataset_dir, self.train_dir, self.query_dir,
-                     self.gallery_dir, self.train_jsonl, self.test_jsonl,
-                     self.query_jsonl]:
-            if not op.exists(path):
-                raise RuntimeError(f"'{path}' is not available")
